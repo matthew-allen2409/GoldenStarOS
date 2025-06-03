426457e6bb62c3b27183694ed28ced4c3b05aad0
matthew-allen2409 <matthew.allen2409@gmail.com>
2025-06-03 07:45:57 -0500

    pass multiboot info struct to kernel_main

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <stdint.h>
 
 #include <kernel/tty.h>
+#include <multiboot/multiboot.h>
 
-void kernel_main(void) {
-	terminal_initialize();
+void kernel_main(uint32_t magic, multiboot_info_t* multiboot_info) {
+    terminal_initialize();
 
-    printf("Hello, World!\n");
+    if (magic != 0x2BADB002) {
+        printf("Invalid magic number: %x\n", magic);
+        return;
+    }
+
+    printf("Hello, World!!\n");
 }
